9d40bfebf62aa76c25956c7cbe64ba5a196e902b
AMMSKI <73558293+AMMSKI@users.noreply.github.com>
2021-04-27 19:38:24 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+// Primitive Value: string, number, boolean, null, undefined (everything not in this list is an object...includeing arrays and functions)
+
+// Object: myObject --> Object.prototype --> null
+// Array: myArray -- Array.prototype --> Object.prototype --> null
+// Function: myFunc --> Function.prototype --> Object.prototype --> null
+// String: myString --> String.prototype --> Object.prototype --> null
+// Number: myNumber --> Number.prototype --> Object.prototype --> null
+// Boolean: myBoolean --> Boolean.prototype --> Object.prototype --> null
+
+const product = 'Computer'
+console.log(product)
+
+const otherProduct = new String('Phone')
+console.log(otherProduct)
